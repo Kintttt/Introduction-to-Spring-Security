ad6ca8a34b07b6e0ec9899263f670fa93386fad9
Kintttt <79329326+Kintttt@users.noreply.github.com>
2022-02-01 13:30:18 +0100

    project completed

---
+++ src/test/java/com/example/springsecurityjavabrains/SpringSecurityJavaBrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjavabrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJavaBrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
